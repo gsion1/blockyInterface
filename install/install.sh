832f9205dc96317bbc0e946fda1410807d7e8758
gsion1 <72071860+gsion1@users.noreply.github.com>
2023-01-01 17:43:52 +0100

    byebye orange pi (welcome rpi)

--- install/install.sh
+++ install/install.sh
@@ -1,39 +1,28 @@
 sudo apt update 
-sudo apt install udhcpd hostapd python3 python3-pip mosquitto mosquitto-clients python3-matplotlib python3-paho-mqtt python3-flask
+sudo apt -y upgrade
+sudo apt install git dnsmasq hostapd python3 python3-pip mosquitto mosquitto-clients python3-matplotlib python3-paho-mqtt python3-flask
 #pip3 install -r ../requirements.txt
 
+cd install
 #Update mosquitto conf to allow access to from other devices
-#sudo sh -c "echo listener 1883 >> /etc/mosquitto/mosquitto.conf"
-#sudo sh -c "echo allow_anonymous true >> /etc/mosquitto/mosquitto.conf"
-#issues with pid files and permissions
 sudo cp mosquitto.conf /etc/mosquitto/mosquitto.conf
 
 #cd ~ && git clone https://github.com/gsion1/blockyInterface.git
-#cd blockyInterface && mkdir server/your_seq
+cd blockyInterface && mkdir server/your_seq
 
-#cd install/
-
-#config files
-#sudo cp hostapd.conf /etc/hostapd/hostapd.conf
-#sudo cp udhcpd.conf /etc/udhcpd.conf
 
 #Add to cron 
 (crontab -l ; echo "@reboot export FLASK_APP=app && cd ~/blockyInterface/server && python3 -m flask run --host 192.168.8.1")| crontab - >
 #sudo (crontab -l ; echo "@reboot mosquitto -v -c /etc/mosquitto/mosquitto.conf")| crontab - #this one is sudo
 
-sudo ./automount_usb.sh #mount usb automatically
 
-#redirect thingva to 198.168.8.1 and reload 
-sudo sh -c "echo 192.168.8.1 thingva >> /etc/hosts"  
-sudo systemctl restart NetworkManager.service
+# not needed for rpi
+#sudo ./automount_usb.sh #mount usb automatically
  
 #redirect port 80 to 5000
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 5000
 sudo iptables-save
 
+echo "please do add to cron (crontab -e)  @reboot export FLASK_APP=app && cd ~/blockyInterface/server && python3 -m flask run --host 192.168.8.1"
+echo "please do add to SUDO cron (crontab -e) @reboot mosquitto -v -c /etc/mosquitto/mosquitto.conf"
 
-
-#switch to root user
-cr =  "@reboot mosquitto -v -c /etc/mosquitto/mosquitto.conf"  #this one is for root user
-sudo (crontab -u root -l; echo "$cr") | crontab -u root
-#echo "please add @reboot mosquitto -v -c /etc/mosquitto/mosquitto.conf to SUDO crontab (sudo crontab -e)"
