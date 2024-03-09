94ae8453f1293d6476e6153fedb3fd2f76fb0ec4
Guillaume Sion <guillaume.sion@hotmail.fr>
2024-03-09 17:16:12 +0100

    print more

--- server/blockly/interface/views.py
+++ server/blockly/interface/views.py
@@ -197,12 +197,14 @@ def find_folder(directory):
     """Recherche le dossier Thingva dans directory."""
     for child_item in os.listdir(directory):
         child_path = os.path.join(directory, child_item)
+        
         if os.path.isdir(child_path) and child_item.lower() == "thingva":
             print("found", child_path)
             return child_path
 
         elif os.path.isdir(child_path):
             found_path = find_folder(child_path)
+            print("testing", child_path, found_path)
             if found_path is not None:
                 print("found", found_path)
                 return found_path
